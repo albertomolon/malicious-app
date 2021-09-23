8310afea571cd0296c805017a83b98a0661588f6
Alberto Molon <alberto.molon97@gmail.com>
2021-09-23 11:00:17 +0200

    Add some library imports

--- app/src/main/java/com/app/malicious/MainActivity.java
+++ app/src/main/java/com/app/malicious/MainActivity.java
@@ -1,6 +1,11 @@
 package com.app.malicious;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.app.Activity;
+import android.widget.LinearLayout;
 
 import android.os.Bundle;
 
